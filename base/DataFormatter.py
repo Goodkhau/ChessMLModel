17d699269789b0fe051de3ab132f02fdcbdaa970
Goodkhau <gos.khai12@gmail.com>
2026-02-04 21:16:02 -0500

    Need to populate keyes then test whether the code works

--- base/DataFormatter.py
+++ base/DataFormatter.py
@@ -1,6 +1,5 @@
 import tensorflow as tf
 from base.ChessSanLexer import ChessSanLexer as lexer
-from base.Piece_Keys import PieceKeys as pk
 
 
 class TrainingData:
@@ -9,18 +8,30 @@ class TrainingData:
         self.san_notation:  list[str] = san_chess_notation
         self.board:         list[list[list[int]]] = [[[0]*8]*8]*8
 
-    def update_board(self, key: list[int]) -> None:
-        return
-    
+    def update_board(self, lex: lexer) -> None:
+        pos: str = lex.san_element
+        L: int = len(pos)
+
+        if pos[0] in ['N','B','R','Q','K']:
+            pos = pos[L-4:L-2] if '+' in pos else pos[L-3:L-1]
+        else:
+            pos = pos[L-4:L-2] if '+' in pos else pos[L-3:L-1]
+        
+        self.board[ ord(pos[0])-97 ][ int(pos[1]) ] += ( lex.chess_piece[0] if self.white_turn else lex.chess_piece[1] )
+
     def san_to_tensorslices(self) -> list[tf.Tensor]:
         set: list[tf.Tensor] = []
 
-        for element in self.san_notation:
-            lex: lexer = lexer(element)
-            if not lex.san_is_valid:
-                continue
+        for index, element in enumerate(self.san_notation):
+            if index == len(self.san_notation)-1:
+                break
 
-            self.update_board(lex.chess_piece[0])
+            lex: lexer = lexer(element)
+
+            if not lex.san_is_valid:
+                return set
+
+            self.update_board(lex)
             set.append(tf.constant(self.board, dtype=tf.int32))
 
         return set
--- base/Piece_Keys.py
+++ base/Piece_Keys.py
@@ -1,12 +1,37 @@
 class PieceKeys:
-    Pawn_Move:     list[list[int]] = [[], []]
-    Pawn_Capture:     list[list[int]] = [[], []]
-    Pawn_Promote:     list[list[int]] = [[], []]
-    Knight:   list[list[int]] = [[], []]
-    Bishop:   list[list[int]] = [[], []]
-    Rook:     list[list[int]] = [[], []]
-    Queen:    list[list[int]] = [[], []]
-    King:     list[list[int]] = [[], []]
+    Pawn_Move:      list[list[int]] = [[], []]
+    Pawn_Capture:   list[list[int]] = [[], []]
+    Pawn_Promote:   list[list[int]] = [[], []]
+    Knight_Move:    list[list[int]] = [[], []]
+    Knight_Capture: list[list[int]] = [[], []]
+    Bishop_Move:    list[list[int]] = [[], []]
+    Bishop_Capture: list[list[int]] = [[], []]
+    Rook_Move:      list[list[int]] = [[], []]
+    Rook_Capture:   list[list[int]] = [[], []]
+    Queen_Move:     list[list[int]] = [[], []]
+    Queen_Capture:  list[list[int]] = [[], []]
+    Queen_Castle:   list[list[int]] = [[], []]
+    King_Move:      list[list[int]] = [[], []]
+    King_Capture:   list[list[int]] = [[], []]
+    King_Castle:    list[list[int]] = [[], []]
 
     def get_chess_key_from_san(self, san_element: str) -> list[list[int]]:
-        return self.Pawn
+        match (san_element[0]):
+            case 'N':
+                return self.Knight_Capture if 'x' in san_element else self.Knight_Move
+            case 'B':
+                return self.Bishop_Capture if 'x' in san_element else self.Bishop_Move
+            case 'R':
+                return self.Rook_Capture if 'x' in san_element else self.Rook_Move
+            case 'Q':
+                if san_element == 'O-O-O':
+                    return self.Queen_Castle
+                return self.Queen_Capture if 'x' in san_element else self.Queen_Move
+            case 'K':
+                if san_element == 'O-O':
+                    return self.King_Castle
+                return self.King_Capture if 'x' in san_element else self.King_Move
+            case _:
+                if any(char in san_element for char in ['N','B','R','Q','K']):
+                    return self.Pawn_Promote
+                return self.Pawn_Capture if 'x' in san_element else self.Pawn_Move
